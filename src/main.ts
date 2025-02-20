72a73923a63f76019a2fbe71b6da0ef774196676
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-02-20 11:32:06 -0300

    added primevue and fonts

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,21 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+// Design System
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
 const app = createApp(App)
 
 app.use(VueAxios, axios)
 app.axios.defaults.baseURL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
 app.provide('axios', app.config.globalProperties.axios)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 
 app.use(createPinia())
 app.use(router)

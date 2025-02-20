71416bee17038cacab3ed3c12394b7114802854b
Alfonso Casajus <60479980+AlfonsoCasajus@users.noreply.github.com>
2025-02-20 10:22:37 -0300

    add axios

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
+// Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 const app = createApp(App)
+
+app.use(VueAxios, axios)
+app.axios.defaults.baseURL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
+app.provide('axios', app.config.globalProperties.axios)
 
 app.use(createPinia())
 app.use(router)
